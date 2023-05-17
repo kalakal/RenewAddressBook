d45d47892dee2f2baf66e198632322a396b84bca
kalakal <sayedfaizanqadri@gmail.com>
2023-05-17 16:01:42 +0530

    Delete a contact using person name problem added

--- src/day3start/AddressBook.java
+++ src/day3start/AddressBook.java
@@ -4,36 +4,90 @@ import java.util.Scanner;
 public class AddressBook {
 
     Contacts contact = new Contacts();
+    static Scanner scanner = new Scanner(System.in);
+        public void addContact() {
+            Scanner scanner = new Scanner(System.in);
 
-    public void addContact() {
-        Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter First Name: ");
+            contact.setFirstName(scanner.nextLine());
 
-        System.out.print("Enter First Name: ");
-        contact.setFirstName(scanner.nextLine());
+            System.out.print("Enter Last Name: ");
+            contact.setLastName(scanner.nextLine());
 
-        System.out.print("Enter Last Name: ");
-        contact.setLastName(scanner.nextLine());
+            System.out.print("Enter Phone Number: ");
+            contact.setPhoneNumber(scanner.nextLine());
 
-        System.out.print("Enter Phone Number: ");
-        contact.setPhoneNumber(scanner.nextLine());
+            System.out.print("Enter Email-ID: ");
+            contact.setEmail(scanner.nextLine());
 
-        System.out.print("Enter Email-ID: ");
-        contact.setEmail(scanner.nextLine());
+            System.out.print("Enter address: ");
+            contact.setAddress(scanner.nextLine());
 
-        System.out.print("Enter address: ");
-        contact.setAddress(scanner.nextLine());
+            System.out.print("Enter City: ");
+            contact.setCity(scanner.nextLine());
 
-        System.out.print("Enter City: ");
-        contact.setCity(scanner.nextLine());
+            System.out.print("Enter State: ");
+            contact.setState(scanner.nextLine());
 
-        System.out.print("Enter State: ");
-        contact.setState(scanner.nextLine());
+            System.out.print("Enter Zip-code: ");
+            contact.setZip(scanner.nextLine());
+        }
+    void editContact() {
 
-        System.out.print("Enter Zip-code: ");
-        contact.setZip(scanner.nextLine());
+
+        System.out.print("\nEnter First Name: ");
+        String firstName = scanner.next();
+
+        if (firstName.equals(contact.getFirstName())){
+
+            System.out.println("Contact Found");
+            System.out.println("Edit Contact Details....");
+
+            System.out.print("Enter First Name: ");
+            contact.setFirstName(scanner.nextLine());
+
+            System.out.print("Enter Last Name: ");
+            contact.setLastName(scanner.nextLine());
+
+            System.out.print("Enter Phone Number: ");
+            contact.setPhoneNumber(scanner.nextLine());
+
+            System.out.print("Enter Email-ID: ");
+            contact.setEmail(scanner.nextLine());
+
+            System.out.print("Enter address: ");
+            contact.setAddress(scanner.nextLine());
+
+            System.out.print("Enter City: ");
+            contact.setCity(scanner.nextLine());
+
+            System.out.print("Enter State: ");
+            contact.setState(scanner.nextLine());
+
+            System.out.print("Enter Zip-code: ");
+            contact.setZip(scanner.nextLine());
+        }
+        else
+            System.out.println("Contact Not Found");
     }
 
-    Contacts displayContact() {
-        return contact;
+
+    void deleteContact(){
+        String firstName = scanner.next();
+
+        System.out.print("\nEnter First Name: ");
+
+        if (firstName.equals(contact.getFirstName())){
+            contact=null;
+            System.out.println("Contact Deleted");
+        }
+        else
+            System.out.println("Contact Not Found");
     }
-}
+
+        Contacts displayContact() {
+            return contact;
+        }
+
+    }
+
